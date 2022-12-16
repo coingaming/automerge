12f300ae6eee062ac1038e7396b31ba275436249
Ekaterina Krivich <krivich.ekaerina@gmail.com>
2022-12-16 16:26:26 +0200

    feat: status is always good

--- dist/index.js
+++ dist/index.js
@@ -144,11 +144,11 @@ class AutomergeAction {
             const baseBranch = pullRequest.base.ref;
             const requiredStatusChecks = yield (0, helpers_1.requiredStatusChecksForBranch)(this.octokit, baseBranch);
             // Only auto-merge if there is at least one required status check.
-            if (requiredStatusChecks) {
-                core.info(`Base branch '${baseBranch}' of pull request ${number} is not sufficiently protected.`);
-                yield this.disableAutoMerge(pullRequest);
-                return;
-            }
+            // if (requiredStatusChecks) {
+            //   core.info(`Base branch '${baseBranch}' of pull request ${number} is not sufficiently protected.`)
+            //   await this.disableAutoMerge(pullRequest)
+            //   return
+            // }
             const labels = pullRequest.labels.map(({ name }) => name).filter(ts_is_present_1.isPresent);
             const doNotMergeLabels = labels.filter(label => this.input.isDoNotMergeLabel(label));
             if (doNotMergeLabels.length > 0) {
